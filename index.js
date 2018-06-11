fac809750ea404a0dd0a5960e4fd3971a6726693
teodor.afrim <teodor.afrim@yahoo.com>
2018-06-11 18:48:05 +0200

    Canvas + chat +socket.io implemented

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+
 const port = process.env.PORT || '3000';
 app.set('port', port);
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static('public'));
 
 const words = require('./resources/words');
 const highscores = require('./resources/highscores');
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     if (err.type === 'entity.parse.failed') {
         return res.status(400).send(JSON.stringify({
             error: {
@@ -36,18 +38,18 @@ app.post('/highscore', (req, res) => {
         }
 
         else {
-            res.status(400).json({message: 'The score for ' + user + ' is not an Integer'});
+            res.status(400).json({ message: 'The score for ' + user + ' is not an Integer' });
             return;
         }
     }
 
     const json = JSON.stringify(highscores);
-    fs.writeFile ("./resources/highscores.json", json, (err) => {
-            if (err) throw err;
-            console.log('complete');
-        }
+    fs.writeFile("./resources/highscores.json", json, (err) => {
+        if (err) throw err;
+        console.log('complete');
+    }
     );
-    res.status(200).json({message: 'Data has been successfully added'});
+    res.status(200).json({ message: 'Data has been successfully added' });
 });
 
 app.get('/words', (req, res) => {
@@ -58,14 +60,14 @@ app.put('/add-word', (req, res) => {
     const wordArray = req.body;
     // Check if data is an array
     if (!Array.isArray(wordArray)) {
-        res.status(400).json({message: 'Data must be a JSON array'});
+        res.status(400).json({ message: 'Data must be a JSON array' });
         return;
     }
 
     for (let newWord in wordArray) {
         // Check if data is in String format
         if (typeof wordArray[newWord] !== 'string') {
-            res.status(400).json({message: 'Data in array must be Strings'});
+            res.status(400).json({ message: 'Data in array must be Strings' });
             return;
         }
 
@@ -81,20 +83,71 @@ app.put('/add-word', (req, res) => {
         else {
             words.push(wordArray[newWord]);
             const json = JSON.stringify(words);
-            fs.writeFile ("./resources/words.json", json, (err) => {
-                    if (err) throw err;
-                    console.log('complete');
-                }
+            fs.writeFile("./resources/words.json", json, (err) => {
+                if (err) throw err;
+                console.log('complete');
+            }
             );
         }
     }
 
-    res.status(200).json({message: 'Data has been successfully added'})
+    res.status(200).json({ message: 'Data has been successfully added' })
 });
 
-/**
- * Start listening
- */
-app.listen(port, () => {
+// Socket setup
+
+const socket = require('socket.io');
+const server = app.listen(port, () => {
     console.log('Listening on port ' + port);
+});
+var io = socket(server);
+
+onlineUsers = [];
+linesHistory = [];
+
+io.on('connection', function (socket) {
+
+    console.log(`Current number of connections: ${io.engine.clientsCount} `);
+    console.log(socket.id);
+
+    //Disconnect
+    socket.on('disconnect', function (data) {
+        console.log(onlineUsers);
+        index = onlineUsers.findIndex(user => user.socketID === socket.id);
+        console.log(index);
+        if(index !== -1) {
+            onlineUsers.splice(index, 1);
+        }
+        console.log('Disconnected: %s sockets connected', io.engine.clientsCount);
+        socket.broadcast.emit('users', onlineUsers);
+    });
+
+    //Send Message
+    socket.on('send message', function (data) {
+        console.log(data, socket.username);
+        let index = onlineUsers.findIndex(user => user.socketID === socket.id);
+        let username = onlineUsers[index].username;
+        io.sockets.emit('new message', { msg: data, user: username });
+    });
+
+    //New User
+    socket.on('new user', function (data) {
+        onlineUsers.push({ socketID: socket.id, username: data, score: 0 });
+        console.log(onlineUsers[onlineUsers.length-1]);
+        io.sockets.emit('users', onlineUsers);
+    });
+
+    //Listen, save and broadcast new drawing elements
+    socket.on('drawing', function (data) {
+        linesHistory.push(data);
+        socket.broadcast.emit('drawing', data);
+    });
+
+    //Send the current drawing
+    socket.on('current drawing', function () {
+        for (var i in linesHistory) {
+            socket.emit('drawing', linesHistory[i]);
+        }
+    });
+
 });
--- public/javascript.js
+++ public/javascript.js
@@ -1,6 +1,6 @@
-
 $(document).ready(function () {
 
+    //AJAX Requests
 
     // TODO: Get the words when is somebody's turn to draw.
     //For now, everytime we click on send word, it gets the data (-> placeholder)
@@ -34,13 +34,13 @@ $(document).ready(function () {
     });
 
     // Get the highscores when the highscore page is ready and update the highscores table.
-    const currentPage = window.location.pathname.split('/')[window.location.pathname.split('/').length-1];
-    if (currentPage=='highscore.html') {
+    const currentPage = window.location.pathname.split('/')[window.location.pathname.split('/').length - 1];
+    if (currentPage == 'highscore.html') {
         $(document).ready(function () {
             $.ajax({
                 type: 'GET',
                 url: 'http://localhost:3000/highscore',
-                success: function(data) {
+                success: function (data) {
                     for (var key in data) {
                         $('#highscoresTable tbody').append(`<tr> <td>${key}</td> <td>${data[key]}</td> </tr>`);
                     }
@@ -51,5 +51,170 @@ $(document).ready(function () {
     }
 });
 
-    // TODO: Update the highscores after every game.
+
+// TODO: Update the highscores after every game.
+
+//Skribbl javascript
+
+
+var socket = io.connect();
+
+var $chatInput = $('#submit');
+var $message = $('#message');
+var $chat = $('#chatMessages');
+var $gameContainer = $('#gameContainer');
+
+var $loginWindow = $('#loginWindow');
+var $userForm = $('#userForm');
+var $users = $('#users tbody');
+var $username = $('#username');
+
+//Submit the Message
+$chatInput.click(function (e) {
+    e.preventDefault();
+    console.log($message.val()); //test
+    socket.emit('send message', $message.val());
+    $message.val(''); //clear it
+});
+
+//Add the new users to our current user playing list
+socket.on('users', function (data) {
+    console.log(data.length);
+    $users.empty();
+    for (i = 0; i < data.length; i++) {
+        console.log(data.length);
+        $users.append(`<tr> <td>${data[i].username}</td> <td>${data[i].score}</td> </tr>`);
+    }
+});
+
+//Add the Message to our Chat Window
+socket.on('new message', function (data) {
+    $chat.append('<p class="well"><strong>' + data.user + '</strong>: ' + data.msg + '</p>');
+});
+
+//Add the new users to our current user playing list
+socket.on('users', function (data) {
+    console.log(data.length);
+    $users.empty();
+    for (i = 0; i < data.length; i++) {
+        console.log(data.length);
+        $users.append(`<tr> <td>${data[i].username}</td> <td>${data[i].score}</td> </tr>`);
+    }
+});
+
+//submit a new User + hide the Login Window + show Game Container
+$userForm.submit(function (e) {
+    e.preventDefault();
+    console.log('Submitted'); //test
+    if ($username.val()) {
+        $loginWindow.addClass('hidden');
+        $gameContainer.addClass('visible');
+        var socket = io.connect();
+        socket.emit('new user', $username.val());
+    }
+    $username.val(''); //clear it
+});
+
+
+
+
+//Canvas Drawing Logic
+
+var canvas = document.getElementById('drawCanvas');
+var context = canvas.getContext('2d');
+var drawContainer = document.getElementById('drawContainer')
+
+var current = {
+    color: 'black'
+};
+
+var next = {
+    x: undefined,
+    y: undefined
+}
+
+var drawing = false;
+
+canvas.addEventListener('mousedown', onMouseDown, false);
+canvas.addEventListener('mouseup', onMouseUp, false);
+canvas.addEventListener('mouseout', onMouseUp, false);
+canvas.addEventListener('mousemove', throttle(onMouseMove, 10), false);
+
+socket.on('drawing', onDrawingEvent);
+
+window.addEventListener('resize', onResize, false);
+
+onResize();
+
+function drawLine(x0, y0, x1, y1, color, emit) {
+    context.beginPath();
+    context.moveTo(x0, y0);
+    context.lineTo(x1, y1);
+    context.strokeStyle = color;
+    context.lineWidth = 2;
+    context.stroke();
+    context.closePath();
+
+    if (!emit) { return; }
+    var w = canvas.width;
+    var h = canvas.height;
+
+    socket.emit('drawing', {
+        x0: x0 / w,
+        y0: y0 / h,
+        x1: x1 / w,
+        y1: y1 / h,
+        color: color
+    });
+}
+
+function onMouseDown(e) {
+    drawing = true;
+    current.x = e.clientX - canvas.offsetLeft;
+    current.y = e.clientY - canvas.offsetTop;
+}
+
+function onMouseUp(e) {
+    if (!drawing) { return; }
+    drawing = false;
+
+    drawLine(current.x, current.y, next.x, next.y, current.color, true);
+}
+
+function onMouseMove(e) {
+    if (!drawing) { return; }
+    next.x = e.clientX - canvas.offsetLeft;
+    next.y = e.clientY - canvas.offsetTop;
+    drawLine(current.x, current.y, next.x, next.y, current.color, true);
+    current.x = e.clientX - canvas.offsetLeft;
+    current.y = e.clientY - canvas.offsetTop;
+}
+
+
+// limit the number of events per second
+function throttle(callback, delay) {
+    var previousCall = new Date().getTime();
+    return function () {
+        var time = new Date().getTime();
+
+        if ((time - previousCall) >= delay) {
+            previousCall = time;
+            callback.apply(null, arguments);
+        }
+    };
+}
+
+function onDrawingEvent(data) {
+    var w = canvas.width;
+    var h = canvas.height;
+    drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color);
+}
+
+
+function onResize() {
+    // Make the canvas fill its parent (will erase it's content)
+    canvas.width = drawContainer.offsetWidth;
+    canvas.height = drawContainer.offsetHeight;
+    socket.emit('current drawing'); //request the current drawing and redraw it
+}
 
